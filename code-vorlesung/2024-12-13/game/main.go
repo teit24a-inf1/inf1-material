84d4b7c08990a82a34a6c375c6cd6b1ea3b213c6
Reiner Huechting <dev@reinerhuechting.de>
2024-12-13 11:10:13 +0100

    Ratespiel-Beispiel hinzugefügt.

---
+++ code-vorlesung/2024-12-13/game/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "inf1-material/code-vorlesung/2024-12-13/guess"
+
+func main() {
+	guess.GuessingGame()
+}
---
+++ code-vorlesung/2024-12-13/guess/guess.go
@@ -0,0 +1,39 @@
+package guess
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func ReadNumber() int {
+	var n int
+	fmt.Println("Rate eine Zahl: ")
+	fmt.Scan(&n)
+	return n
+}
+
+func GuessingGame() {
+	// Verwende einen Zufallsgenerator,
+	// um die erwartete Zahl zu bestimmen.
+	expectedGuess := rand.Intn(16) + 1
+	for i := 0; i < 3; i++ {
+		g := ReadNumber()
+		if NumberGood(expectedGuess, g) {
+			fmt.Println("Richtig geraten")
+			return
+		}
+		// Bei falscher Antwort einen Hinweis geben,
+		// ob die Zahl zu groß oder zu klein war.
+		if expectedGuess < g {
+			fmt.Println("Zu groß")
+		} else {
+			fmt.Println("Zu klein")
+		}
+
+	}
+	fmt.Println("Drei Mal falsch geraten")
+}
+
+func NumberGood(expected, n int) bool {
+	return n == expected
+}

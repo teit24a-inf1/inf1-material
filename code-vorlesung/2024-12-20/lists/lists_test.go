b4894b2e436695329c0c51faafef0f3587b7533f
Tomderandere <tompbickle@gmail.com>
2024-12-20 11:19:03 +0100

    n

--- code-vorlesung/2024-12-20/lists/lists_test.go
+++ code-vorlesung/2024-12-20/lists/lists_test.go
@@ -11,14 +11,28 @@ func Example_lists() {
 	// p1 := 0
 	// p2 := 0
 
-	students := []string{"Max Mustermann", "Bertha Beispiel"}
-	points := []int{0, 0}
-	// in geschweifter klammer inizialisierungswerte
+	students := []string{"Max Mustermann", "Bertha Beispiel"} // mit [2] wäre es ein array
+	points := []int{0, 0}                                     // in geschweifter klammer inizialisierungswerte
+	// Gib beide Listen als Ganzes aus.
 	fmt.Println(students)
 	fmt.Println(points)
+
+	// Zugriff auf einzelne Elemente
+	s1 := students[0]
+	s2 := students[1]
+	//s3 := students[2] //zugriff auf nichz existierende elemente würden eine Panic auslösen
+	fmt.Println(s1)
+	fmt.Println(s2)
+	//fmt.Println(s3)
 	// in den Output schreibt man was der Test erwarten soll
 	// alle dateien mit _test.go als end werden als test erkannt. Functionen werden mit example oder test am anfang als test erkannt
+
+	// EIne SChleife, die durch alle Elemente der Liste läuft
+	// und sie ausgibt
+	// Dazu wird ein selbst verwalteter Schleifenzähler benutzt.
+	for i := 0; i < len(students); i++ {
+		fmt.Println(students[i])
+	}
 	// Output:
-	//[Max Mustermann Bertha Beispiel]
-	//[0 0]
+
 }

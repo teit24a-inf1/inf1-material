bf97684c81404d11a2f787e1aca740c36d23a702
Reiner Huechting <dev@reinerhuechting.de>
2025-01-17 11:13:36 +0100

    Fehler-Beispiele aus einer Klausur hinzugefügt.

---
+++ code-vorlesung/2025-01-17/klausur-fehler-beispiele/aufgaben/aufgabe1/aufgabe1.go
@@ -0,0 +1,14 @@
+package aufgabe1
+
+/* AUFGABENSTELLUNG: Vervollständigen Sie die Funktion EvenPrefix.
+ * ERREICHBARE PUNKTE: 10
+ */
+
+// EvenPrefix erwartet eine Liste von Zahlen und liefert
+// die längste Anfangs-Folge, die nur gerade Zahlen enthält.
+// D.h. eine Liste mit allen geraden Zahlen vor der ersten ungeraden.
+func EvenPrefix(list []int) []int {
+	result := []int{}
+	// TODO
+	return result
+}

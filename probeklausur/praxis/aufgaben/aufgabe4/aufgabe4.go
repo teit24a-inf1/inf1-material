df4c9ceb4755126fedcc3d6c427831871706f486
Tomderandere <tompbickle@gmail.com>
2025-02-25 11:12:26 +0100

    aufgabe 4

--- probeklausur/praxis/aufgaben/aufgabe4/aufgabe4.go
+++ probeklausur/praxis/aufgaben/aufgabe4/aufgabe4.go
@@ -10,6 +10,24 @@ ZUSATZBEDINGUNG: Die Funktion muss rekursiv sein!
 // jeweils das größere der beiden Elemente enthält.
 // Falls eine Position nur in einer Liste vorkommt, gilt dieses Element als das größere.
 func MaxElements(l1, l2 []int) []int {
-	// TODO
-	return []int{}
+
+	if len(l1) == 0 && len(l2) == 0 {
+		return []int{}
+	}
+	if len(l1) != 0 && len(l2) == 0 {
+		return l1
+	}
+	if len(l1) == 0 && len(l2) != 0 {
+		return l2
+	}
+	result := MaxElements(l1[1:], l2[1:])
+
+	if l1[0] >= l2[0] {
+		result = append([]int{l1[0]}, result...)
+	}
+	if l1[0] <= l2[0] {
+		result = append([]int{l2[0]}, result...)
+	}
+
+	return result
 }
